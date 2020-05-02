8c52d25cbe0622dda951f1e5f59f66b8472fa1e6
Abigail Nielson <aa.nielson18@gmail.com>
2020-05-01 17:26:51 -0700

    added variables for user input and set up command structure

--- liri.js
+++ liri.js
@@ -2,34 +2,51 @@
 require("dotenv").config();
 //Add the code required to import the keys.js file and store it in a variable.
 var keys = require("./keys.js");
-// access your keys information
+// access your Spotify keys information
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 //commands to take in
+var filesSystem = require("fs"); //included for the fourth command that utilizes the txt file
+const axios = require('axios'); //included for the queries
+var command = process.argv[2]; //user input to decide on which command
+var name = process.argv[3]; //info to be queried
+
+var runCommands = function {
     //concert-this
+    if (command === "concert-this") {
         //search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist
+        
         //render the venueName, venueLocation, & eventDate (use moment to format this as "MM/DD/YYYY") about each event to the terminal
         //no need to sign up for a Bands in Town api_id key.
             //Use the "codingbootcamp" as your "app_id"
             //ex url: https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp
-        
+    }  
+
     //spotify-this-song
-    //You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API
-        //The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
-        // Step One: Visit https://developer.spotify.com/my-applications/#!/
-        // Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-        // Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-        // Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
-        
+    if (command === "spotify-this-song") {
+        //You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API
         //ex command: node liri.js spotify-this-song '<song name here>'
-        //show the following information about the song in your terminal/bash window
+        spotify.search(
+            {
+                type: "track",
+                query: name
+            }
+        ).then(function(response) {
+            //show the following information about the song in your terminal/bash window
+            
             //artists
             //song name
             //preview link of the song from spotify
             //album that the song is from
             //If no song is provided then your program will default to "The Sign" by Ace of Base
+            }
+        })
+        
+        
 
     //movie-this
+    if (command === "movie-this") {
         //You'll use the axios package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy
         //ex command line: node liri.js movie-this '<movie name here>'
         //output the folling info to the terminal/bash windo:
@@ -42,10 +59,27 @@ var spotify = new Spotify(keys.spotify);
             //   * Plot of the movie.
             //   * Actors in the movie.
             //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+    }
 
     //do-what-it-says
+    if (command === "do-what-it-says") {
         //ex command line: node liri.js do-what-it-says
-        //Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-        // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-        // Edit the text in random.txt to test out the feature for movie-this and concert-this.
-
+        doIt();
+        // use the txt to call one of LIRI's commands.
+        runCommands();
+    }
+}
+//Using the fs Node package, LIRI will take the text inside of random.txt
+function doIt() {
+    // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+    // Edit the text in random.txt to test out the feature for movie-this and concert-this.
+    filesSystem.readFile("random.txt", "utf8", function(err,data) {
+        if (err) {
+            console.log(err);
+        } 
+        output = data.split(", ");
+        command = data[0];
+        name = data[1];
+        return command, name;
+    }); 
+} 
