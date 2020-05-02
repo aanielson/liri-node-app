e1a657a10acf45f217363df32388c792e9b99030
Abigail Nielson <aa.nielson18@gmail.com>
2020-05-01 19:28:25 -0700

    added in the axios calls for concert-this and movie-this. still need to update null conditions and moment for concert-this

--- liri.js
+++ liri.js
@@ -1,18 +1,19 @@
 //add code to read and set any environment variables with the dotenv package:
 require("dotenv").config();
 
+//Necessary requirements for the spotify queries
 //Add the code required to import the keys.js file and store it in a variable.
 var keys = require('./keys.js'); //not currently working...
-
 // access your Spotify keys information
 var Spotify = require('node-spotify-api');
 var spotifySearch = new Spotify(keys.spotify);
 console.log(spotifySearch.id);
 
-
-//commands to take in
+//Necessary requirements for the file integration and axios call
 var filesSystem = require("fs"); //included for the fourth command that utilizes the txt file
-const axios = require('axios'); //included for the queries
+const axios = require('axios').default; //included for the movie query
+
+//take in user input and store as variables for the queries
 var command = process.argv[2]; //user input to decide on which command
 var name = process.argv[3]; //info to be queried
 
@@ -21,14 +22,21 @@ var runCommands = function() {
     if (command === "concert-this") {
         var artist = name;
         //search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist
-        
+        var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        //axios call
+        axios({
+            method: "get",
+            url: queryURL,
+            responseType: "stream"
+        }).then(function (response) {
         //render the venueName, venueLocation, & eventDate (use moment to format this as "MM/DD/YYYY") about each event to the terminal
-        for (var i = 0; i < 3; i++) {
-            console.log(response.venue.name);
-            console.log(response.venue.location);
-            console.log(response.datetime);
-        }
-    }  
+            for (var i = 0; i < 3; i++) {
+                console.log(response.venue.name);
+                console.log(response.venue.location);
+                console.log(response.datetime);
+            }
+        })
+    }; 
 
     //spotify-this-song
     if (command === "spotify-this-song") {
@@ -37,24 +45,25 @@ var runCommands = function() {
         spotifySearch.search(
             {
                 type: "track",
-                query: name
+                query: name,
+                limit: 5
+        }, function(err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
             }
-        ).then(function(response) {
             //show the following information about the song in your terminal/bash window
-            console.log(response.tracks);
+            console.log(data);
             //artists
-            JSON.stringify(response.tracks.items[0].artists[0].name);
+            JSON.stringify(data.tracks.items[0].artists[0].name);
             //song name
-            //JSON.stringify(response.tracks.items[0]);
+            JSON.stringify(data.tracks.items[0]);
             //preview link of the song from spotify
-            JSON.stringify(response.tracks.items[0].external_urls[0].href);
+            JSON.stringify(data.tracks.items[0].external_urls[0].href);
             //album that the song is from
-            JSON.stringify(response.tracks.items[0].name);
+            JSON.stringify(data.tracks.items[0].name);
             //If no song is provided then your program will default to "The Sign" by Ace of Base
-        }).catch(function(err) {
-            console.log(err);
         });
-    }
+    };
         
     //movie-this
     if (command === "movie-this") {
@@ -63,17 +72,30 @@ var runCommands = function() {
         var movieName = name;
         // Then run a request with axios to the OMDB API with the movie specified
         var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-        
+        axios({
+            method: "get",
+            url: queryUrl,
+            responseType: "stream"
+        }).then(function (response) {
         //output the folling info to the terminal/bash windo:
             // Title of the movie.
+            console.log(response.data.Title);
             // Year the movie came out.
+            console.log(response.data.Released);
             // IMDB Rating of the movie.
+            console.log(response.data.imdbRating);
             // Rotten Tomatoes Rating of the movie.
+            console.log(response.data.Ratings[1]);
             // Country where the movie was produced.
+            console.log(response.data.Country);
             // Language of the movie.
+            console.log(response.data.Language);
             // Plot of the movie.
+            console.log(response.data.Plot);
             // Actors in the movie.
+            console.log(response.data.Actors);
             //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+        });
     }
 
     //do-what-it-says
